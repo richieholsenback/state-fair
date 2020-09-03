2e04869ead0fdc8c018fb3b9c8c04a47566de77f
Richie Holsenback <richieholsenback@gmail.com>
2020-09-03 13:33:06 -0500

    added all buttons with functionality

--- scripts/TicketCount/TicketCounter.js
+++ scripts/TicketCount/TicketCounter.js
@@ -1,5 +1,9 @@
 const contentTarget = document.querySelector(".ticketCounter")
 const eventHub = document.querySelector("#state-fair")
+let ticketCount = 0
+const ticketCounter = (numOfTickets) => {
+    
+}
 
 export const ShowTicketHolders = () => {
     eventHub.addEventListener("showTicketPurchased", clickEvent => {
