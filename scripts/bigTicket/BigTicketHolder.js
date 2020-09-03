3d74b4aef312944484c2884163cfd2d87d2ac79c
Richie Holsenback <richieholsenback@gmail.com>
2020-09-03 09:11:37 -0500

    fixed big spender button

--- scripts/bigTicket/BigTicketHolder.js
+++ scripts/bigTicket/BigTicketHolder.js
@@ -1,9 +1,14 @@
-const contentTarget = document.querySelector(".people")
+const contentTargetFood = document.querySelector(".food")
+const contentTargetGames = document.querySelector(".games")
+const contentTargetRides = document.querySelector(".rides")
+const contentTargetSideshow = document.querySelector(".sideshow")
 const eventHub = document.querySelector("#state-fair")
 
 export const BigTicketHolders = () => {
     eventHub.addEventListener("bigTicketPurchased", clickEvent => {
-        contentTarget.innerHTML += `<div class="person bigSpender"></div>`
-
+        contentTargetFood.innerHTML += `<div class="person bigSpender"></div>`
+        contentTargetGames.innerHTML += `<div class="person bigSpender"></div>`
+        contentTargetRides.innerHTML += `<div class="person bigSpender"></div>`
+        contentTargetSideshow.innerHTML += `<div class="person bigSpender"></div>`
     })
 }
